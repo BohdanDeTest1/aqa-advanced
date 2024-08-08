a549f89b6ac452300275146e0c48f217fa6439ea
Bohdan Derba <derba.se.b@gmail.com>
2024-08-08 18:09:44 +0300

    Files for homework 3

---
+++ git_basics/jsBasics/task2.js
@@ -0,0 +1,4 @@
+let nameOne = "Jack";
+let nameTwo = "John";
+
+console.log("Dear " + nameOne + " & " + nameTwo + ", take my congratulations");
---
+++ git_basics/jsBasics/task4.js
@@ -0,0 +1,16 @@
+// task 1
+let circleRadiusOne = 5;
+const circleSquareOne = Math.PI * Math.pow(circleRadiusOne, 2);
+console.log(circleSquareOne.toFixed(2))
+
+// task 2
+let length = 11.3;
+const width = 0.99;
+const Square = length * width;
+console.log(Square.toFixed(2))
+
+// task 3
+let circleRadiusTwo = 14.35;
+let height = 2
+const circleSquareTwo = Math.PI * Math.pow(circleRadiusTwo, 2) * height;
+console.log(circleSquareTwo.toFixed(2))
