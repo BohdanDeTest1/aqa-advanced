918f5022cc767470c12cd11efd2ed33216783e7d
Bohdan Derba <derba.se.b@gmail.com>
2024-08-08 22:29:06 +0300

    Files for homework 5

---
+++ git_basics/homework_folder_5/task3.js
@@ -0,0 +1,17 @@
+//for cycle "For"
+let number = 3;
+let f = "For";
+let w = "While";
+console.log(`\r\n --- Table for cycle "${f}" ---\r\n`)
+for (let i = 1 ; i <=10 ; i++){
+    console.log(`${number} * ${i} = ${number*i}`)
+}
+
+//for cycle "While"
+console.log(`\r\n--- Table for cycle "${w}" ---\r\n`)
+let numberTwo = 4;
+let i = 0;
+while (i<10){
+    i++
+    console.log(`${numberTwo} * ${i} = ${numberTwo*i}`)
+}
