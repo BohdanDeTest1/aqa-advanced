721699ddcbd25f3f44270752c9fb579d49eff586
Bohdan Derba <derba.se.b@gmail.com>
2024-08-18 21:29:12 +0300

    Files for homework -functions

---
+++ git_basics/homework_functions/task2.js
@@ -0,0 +1,13 @@
+function muttureness (age){
+    if(age>=18){
+        return true
+    }else{
+        return false
+    }
+    }
+    let age1 = 15
+    let age2 = 25
+    console.log(`Test age is ${age1}. Does they matture: ${muttureness(age1)}`);
+    console.log(`Test age is ${age2}. Does they matture: ${muttureness(age2)}`);
+
+    
